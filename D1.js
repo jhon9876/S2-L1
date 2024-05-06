ce9cfb1ee5f898f06757c9c6311c722cb5babf2d
Giovanni Iacono <98giovanniiacono98@gmail.com>
2024-05-06 17:40:43 +0200

    Update D1.js

--- D1.js
+++ D1.js
@@ -66,8 +66,8 @@ let name2="John"
 const Uguaglianza= (name1===name2)? "Uguali" : "Diverse"
 console.log("Le due stringhe sono: " + Uguaglianza)
 // non compreso bene l'extra
-name1.toLowerCase()
+name1=name1.toLowerCase()
 console.log("Stringa name1 in lowercase: " + name1)
-name2.toLowerCase()
+name2=name2.toLowerCase()
 console.log("Stringa name2 in lowercase: " + name2)
 console.log("Le due stringe in lowercase sono uguali? : " + name1===name2)
