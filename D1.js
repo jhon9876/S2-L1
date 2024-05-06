9b0c04d58a9e3136e7d69aafdfaa71091e6a0ca3
Giovanni Iacono <98giovanniiacono98@gmail.com>
2024-05-06 17:56:10 +0200

    Update D1.js

--- D1.js
+++ D1.js
@@ -15,31 +15,30 @@ REGOLE
 // In javascript, come in ogni altro linguaggio di programmazione, sono presenti vari tipi di dato tra cui troviamo:
 // 1) Tipo numerico (intero o in virgola mobile)
 // 2) Tipo stringa (Es. "Mario")
-// 3)Tipo bool (True o False utilizzato in genere all'interno di costrutti per verificare determinate condizioni es:if) 
+// 3)Tipo bool (True o False utilizzato in genere all'interno di costrutti per verificare determinate condizioni es:if)
 /* ESERCIZIO 2
  Crea una variable chiamata "myName" e assegna ad essa il tuo nome, sotto forma di stringa.
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
-const myName= "Giovanni"
+const myName = "Giovanni";
 console.log("Il mio nome è: " + myName);
 /* ESERCIZIO 3
  Scrivi il codice necessario ad effettuare un addizione (una somma) dei numeri 12 e 20.
 */
 
-
 /* SCRIVI QUI LA TUA RISPOSTA */
-const primoNumero=12
-const secondoNumero=20
-const somma=primoNumero+secondoNumero
-console.log("la somma è data da: " + somma)
+const primoNumero = 12;
+const secondoNumero = 20;
+const somma = primoNumero + secondoNumero;
+console.log("la somma è data da: " + somma);
 /* ESERCIZIO 4
  Crea una variable di nome "x" e assegna ad essa il numero 12.
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
-let x=12
-console.log("Il valore di x è: " + x)
+let x = 12;
+console.log("Il valore di x è: " + x);
 /* ESERCIZIO 5
   Riassegna un nuovo valore alla variabile "myName" già esistente: il tuo cognome.
   Dimostra l'impossibilità di riassegnare un valore ad una variabile dichiarata con il costrutto const.
@@ -52,8 +51,8 @@ console.log("Il valore di x è: " + x)
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
-sottrazione=4 - x
-console.log("Il valore della sottrazione è: " + sottrazione)
+const sottrazione = 4 - x;
+console.log("Il valore della sottrazione è: " + sottrazione);
 /* ESERCIZIO 7
  Crea due variabili: "name1" e "name2". Assegna a name1 la stringa "john", e assegna a name2 la stringa "John" (con la J maiuscola!).
  Verifica che name1 sia diversa da name2 (suggerimento: è la stessa cosa di verificare che la loro uguaglianza sia falsa).
@@ -61,13 +60,13 @@ console.log("Il valore della sottrazione è: " + sottrazione)
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
-let name1="john"
-let name2="John"
-const Uguaglianza= (name1===name2)? "Uguali" : "Diverse"
-console.log("Le due stringhe sono: " + Uguaglianza)
+let name1 = "john";
+let name2 = "John";
+const uguaglianza = name1 === name2 ? "Uguali" : "Diverse";
+console.log("Le due stringhe sono: " + uguaglianza);
 // non compreso bene l'extra
-name1=name1.toLowerCase()
-console.log("Stringa name1 in lowercase: " + name1)
-name2=name2.toLowerCase()
-console.log("Stringa name2 in lowercase: " + name2)
-console.log("Le due stringe in lowercase sono uguali? : " + name1===name2)
+// name1 = name1.toLowerCase();
+// console.log("Stringa name1 in lowercase: " , name1);
+// name2 = name2.toLowerCase();
+// console.log("Stringa name2 in lowercase: " , name2);
+console.log("Le due stringe in lowercase sono uguali? : ", name1.toLowerCase() === name2.toLowerCase());
